48d14a801cb77642279c5aaf574915a6be4b8e87
Lucas Roberto de Oliveira Barcelos <lucasolbarcelos@gmail.com>
2022-01-20 20:49:19 -0300

    Atualização

--- generation/src/program/Application.java
+++ generation/src/program/Application.java
@@ -10,24 +10,33 @@ public class Application {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner (System.in);
 		
-		int salario, filho;
-		double mediaSalario, mediaFilhos;
-		
-		System.out.print("Digite um salário: ");
-		salario = sc.nextInt();
-		
-		int totalSalario = 0;
-		for (int i=1; i<3; i++) {
-			
-			totalSalario = totalSalario + salario;
+		int salario, filho, totalSalario = 0, numFilhos = 0,maior = 0,cont = 0;
+		double mediaSalario, mediaFilhos, percentual;
+
+		for (int i=0; i<3; i++) {	  
 			System.out.print("Digite um salário: ");
-			salario = sc.nextInt();
-			
+			salario = sc.nextInt();    //entrar com o valor do salário
+			totalSalario += salario;  //guardar o valor dos salários para calcular a média
+			if (salario >maior) {    // calcular qual é o maior
+				maior = salario;
+			}	
+			if (salario<100) {//    verificar quantas pessoas ganham menos de R$ 100,00
+				cont += 1;
+			}
+			System.out.print("Digite quantos filhos a pessoa tem: ");
+			filho = sc.nextInt();  // entrar com o número de filhos
+			numFilhos += filho;	
 		}
-		System.out.println(totalSalario);
+		
+		mediaSalario = totalSalario/3.0;    //media salarial 
+		mediaFilhos = numFilhos/3.0;    //media de filhos
+		percentual = (cont/3.0)*100.00;
 		
 		
-		
+		System.out.printf("A média do salário da população é: %.2f%n", mediaSalario);
+		System.out.printf("A média do número de filhos é: %.2f%n", mediaFilhos); 
+		System.out.println("O maior salário é: R$ " + maior); 
+		System.out.printf("O percentual de pessoas com salário até R$100,00 é de: %.2f%n",percentual); 	
 		
 		sc.close();
 	}
